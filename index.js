b9479872cc72a73f530f8ac320d597cc0e672223
lautaro <lauferreyraff@gmail.com>
2022-08-25 19:31:01 -0300

    esquemacion js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let resultado = document.getElementById("cuadroResultado");
+let encriptar = document.getElementById("encriptar");
+               encriptar.addEventListener("click", encriptartx);
+let desencriptar = document.getElementById("desencriptar");
+               desencriptar.addEventListener("click", desencriptartx);
+function encriptartx(){
+    let texto = document.getElementById("texto").value;
+    for(i=0;length(texto);i++){
+
+    }
+}
+function desencriptartx(){
+    let texto = document.getElementById("texto").value;
+    for(i=0;length(texto);i++){
+
+    }
+}
+
 
