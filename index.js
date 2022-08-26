673a20f12fd8d16c3d71dd0a9ca893cd2ff5213a
lautaro <lauferreyraff@gmail.com>
2022-08-26 19:44:32 -0300

    funcionalidad javascript

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let resultado = document.getElementById("cuadroResultado");
-let encriptar = document.getElementById("encriptar");
+
+let encrip = document.getElementById("encriptar");
                encriptar.addEventListener("click", encriptartx);
-let desencriptar = document.getElementById("desencriptar");
+let desencrip = document.getElementById("desencriptar");
                desencriptar.addEventListener("click", desencriptartx);
+
 function encriptartx(){
-    let texto = document.getElementById("texto").value;
-    for(i=0;length(texto);i++){
+    let texto = document.getElementById("texto").value.toLowerCase();
+    if (texto != ""){
+    let textofinal = texto.replace(/e/img, "emter").replace(/i/img, "imes").replace(/a/img, "ai").replace(/o/img, "ober").replace(/ufat/img, "ugat");
 
+    document.getElementById("cuadroResultado").innerHTML = textofinal;
     }
 }
+    
+
+
+
 function desencriptartx(){
-    let texto = document.getElementById("texto").value;
-    for(i=0;length(texto);i++){
+    let texto = document.getElementById("texto").value.toLowerCase();
+    if (texto != ""){
+    let textofinal = texto.replace(/enter/img, "e").replace(/imes/img, "i").replace(/ai/img, "a").replace(/ober/img, "o").replace(/ufat/img, "u");
 
+    document.getElementById("cuadroResultado").innerHTML = textofinal;
+   }
     }
-}
+   
 
 
