1654947e3faf08dff97996f8aca6e19a9f070248
lautaro <lauferreyraff@gmail.com>
2022-08-26 21:17:26 -0300

    funcion boton copiar terminada, solucion errores, version final

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function encriptartx(){
     let texto = document.getElementById("texto").value.toLowerCase();
     if (texto != ""){
     let textofinal = texto.replace(/e/img, "emter").replace(/i/img, "imes").replace(/a/img, "ai").replace(/o/img, "ober").replace(/ufat/img, "ugat");
+    document.getElementById("cuadroResultado").innerHTML = ""
 
-    document.getElementById("cuadroResultado").innerHTML = textofinal;
+    document.getElementById("textoTransformado").innerHTML = textofinal;
     }
 }
     
@@ -20,10 +21,10 @@ function desencriptartx(){
     let texto = document.getElementById("texto").value.toLowerCase();
     if (texto != ""){
     let textofinal = texto.replace(/enter/img, "e").replace(/imes/img, "i").replace(/ai/img, "a").replace(/ober/img, "o").replace(/ufat/img, "u");
+    document.getElementById("cuadroResultado").innerHTML =""
 
-    document.getElementById("cuadroResultado").innerHTML = textofinal;
+    document.getElementById("textoTransformado").innerHTML = textofinal;
    }
-    }
-   
+}
 
 
